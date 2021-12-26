eea9b1fdca53220a1cba2f025afddc206dd5b7bf
Nisarg Mewada <nisargmewada@gmail.com>
2021-12-26 14:23:03 +0100

    adding email validation

--- src/main/java/com/wanzl/aufgabe/registration/Registration.java
+++ src/main/java/com/wanzl/aufgabe/registration/Registration.java
@@ -3,6 +3,8 @@ package com.wanzl.aufgabe.registration;
 import com.wanzl.aufgabe.Benutzer;
 
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Registration implements IRegistrierung {
@@ -23,5 +25,21 @@ public class Registration implements IRegistrierung {
         UUID uuid = UUID.randomUUID();
         System.out.println(uuid);
         return null;
+    }
+    public static class EmailValidator{
+        private final Pattern pattern;
+
+        private static final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        public EmailValidator()
+        {
+            pattern = Pattern.compile(EMAIL_REGEX);
+        }
+        public boolean validate(final String email)
+        {
+            Matcher matcher = pattern.matcher(email);
+            return matcher.matches();
+        }
     }
 }
